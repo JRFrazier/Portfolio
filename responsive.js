f9c756f9d13eebb8cf1e324a29a18f627da0cb64
Justin Frazier <Justin Frazier>
2017-07-01 07:55:53 -0400

    changed styles sheets on all pages

--- responsive.js
+++ responsive.js
@@ -1,3 +1,4 @@
+/* intro fade */
 $(document).ready(function() { 
 
     $(".container").niceScroll();
@@ -10,14 +11,4 @@ $(window).scroll(function(){
     $(".intro").css("opacity", 1 - $(window).scrollTop() / 250);
   });
 
-  /* Sticky Navbar */
-
-  /*$(window).scroll(function () {
-
-    if ($(window).scrollTop() > 280) {
-      $('#nav_bar').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 281) {
-      $('#nav_bar').removeClass('navbar-fixed');
-    }
-  });*/
+  
