6f98271f89d9edaa31450da87a815f5ed1b660a5
Justin Frazier <Justin Frazier>
2017-07-01 03:22:05 -0400

    fixed nav bar

--- responsive.js
+++ responsive.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
 $(window).scroll(function(){
     $(".intro").css("opacity", 1 - $(window).scrollTop() / 250);
   });
+
+  /* Sticky Navbar */
+
+  /*$(window).scroll(function () {
+
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });*/
